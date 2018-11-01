982a5bccfb5d4af298fa20cd05b88c653776499a
Szendrényi Ákos <Szendrnyi kos@DESKTOP-SG6VCUU>
2018-11-01 12:43:56 +0100

    Bekérés

--- src/feladat_1/Feladat_1.java
+++ src/feladat_1/Feladat_1.java
@@ -1,11 +1,23 @@
 
 package feladat_1;
-
+import java.util.Scanner;
 
 public class Feladat_1 {
 
    
     public static void main(String[] args) {
+        
+        System.out.println("Kérem adja meg a medence átmérőjét: ");
+       
+        
+       
+         System.out.println("Kérem adja meg a medence mélységét: ");
+        
+ 
+       
+ 
+        System.out.println("A medencébe enyyi köbméter víz fér bele: ");
+
              
     }
     
